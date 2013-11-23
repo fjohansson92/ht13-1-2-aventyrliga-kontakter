b0c76fca1c194ae7c3f233c720842ec98cd0e720
Your Name Here <fj222dr@student.lnu.se>
2013-11-23 10:39:17 +0100

    Skapat anslutning till databasen

---
+++ AdventurousContacts/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AdventurousContacts.Models
+{
+    [MetadataType(typeof(Contact_Metadata))]
+    public partial class Contact
+    {
+        public class Contact_Metadata
+        {
+            //TODO: Add validation
+            public string EmailAdress { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
