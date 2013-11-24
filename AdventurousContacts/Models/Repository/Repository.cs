116c6464f38f2b5d9b24c192cc6d14b98c49e2e6
Your Name Here <fj222dr@student.lnu.se>
2013-11-24 11:27:53 +0100

    Lagt till funktionalitet för att arbeta med de lagrade procedurerna

---
+++ AdventurousContacts/Models/Repository/Repository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using AdventurousContacts.Models.DataModels;
+
+namespace AdventurousContacts.Models.Repository
+{
+    public class Repository
+    {
+        private bool _disposed = false;
+
+        private AdventureWorksAssignmentEntities _entites = new AdventureWorksAssignmentEntities();
+
+
+        public void Add(Contact contact)
+        {
+            _entites.Contacts.Add(contact);
+        }
+
+        public void Delete(Contact contact)
+        {
+            _entites.Contacts.Remove(contact);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    _entites.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public IQueryable<Contact> FindAllContacts()
+        {
+            return _entites.Contacts;
+        }
+
+        public Contact GetContactById(int contactId)
+        {
+            return _entites.Contacts.Find(contactId);
+        }
+
+        public List<Contact> GetLastContacts(int count)
+        {
+            return _entites.Contacts.Take(count).ToList();
+        }
+
+        public void Save()
+        {
+            _entites.SaveChanges();
+        }
+
+        public void Update(Contact contact)
+        {
+            _entites.Entry(contact).State = EntityState.Modified;
+        }
+
+
+    }
+}
